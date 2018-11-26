9ac43ca8ff27ac467ca30b91f7cc1e42888e11fe
andro951 <andro951@gmail.com>
2018-11-26 00:14:42 -0500

    Added comments

--- Putting everything together OOP/Putting everything together OOP/Objects.h
+++ Putting everything together OOP/Putting everything together OOP/Objects.h
@@ -7,6 +7,7 @@ using namespace std;
 class Object
 {
 public:
+	//Comments explaining the static variables can be found in Objects.cpp.
 	static int s_Time;
 	static int* s_pTime;
 	static bool s_RadiatorAttatchedToFloor;
@@ -20,17 +21,17 @@ public:
 	static bool s_Called911;
 	static int s_TimeCalledPolice;
 
-	void setCommandList(string commandList);
-	void examine();
-	void listCommands();
-	void setExamineText(string examineText);
-	void setExamineTime(int examineTime);
-	void breakRadiator();
-	void pickUp();
-	void drop();
+	void setCommandList(string commandList); //Set the string that is displayed for the object when commands for object is input as the command object.
+	void examine(); //Display a simple description of the object.
+	void listCommands(); //Display the string set by setCommandsList.
+	void setExamineText(string examineText); //Set the description of the object.
+	void setExamineTime(int examineTime); //Set the ammount of time required to examine the object.
+	void breakRadiator(); //Breaks the radiator and allows the player to move around the room.
+	void pickUp(); //The player picks up the object if is was previously broken.
+	void drop(); //The player drops the object if they were holding it.
 private:
-	
-	string m_ExamineText;
-	int m_ExamineTime;
-	string m_CommandList;
+	//Private used to prevent other these variables from being directly manipulated without accesing the class's member functions.
+	string m_ExamineText; //Stores the examine text for the object.
+	int m_ExamineTime; //Stores the time requiered to examine the object.
+	string m_CommandList; //Stores the list of available commands for the object.
 };
